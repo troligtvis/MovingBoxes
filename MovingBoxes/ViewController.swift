2ddcae9c5214b58da0b3f7fe0d5d90318a553e4d
strawberrypie <kjdr@kth.se>
2014-12-07 23:04:21 +0100

    Initial Commit

---
+++ MovingBoxes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovingBoxes
+//
+//  Created by Kj Drougge on 2014-12-07.
+//  Copyright (c) 2014 kj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
